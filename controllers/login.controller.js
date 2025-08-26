d510c0e6e6e97471b9d37d085faf86b4862a88cb
asmaa <20231700208@cis.asu.edu.eg>
2025-08-26 13:06:45 +0300

    tokenLogin

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,6 +1,6 @@
-// Example login controller
 const bcrypt = require('bcryptjs');
 const User = require('../models/users.model');
+const jwt = require('jsonwebtoken');
 
 exports.login = async (req, res) => {
   try {
@@ -10,9 +10,13 @@ exports.login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
-
-    // Generate JWT or session here if needed
-    res.status(200).json({ message: 'Login successful', userId: user._id });
+    // tokenn
+    const token = jwt.sign(
+      { userId: user._id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ message: 'login successful',token: token, userId: user._id });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
